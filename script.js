0c2f0e1c8100d6e1d32afd084fcb7ee82341c6f1
Carolina Strecht <carolina.stsampaio@gmail.com>
2021-12-07 17:32:08 +0000

    enable drawing mode

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-// Select a color, and save that choice:
-let actualColor = "black"; // start always w/ black color
+let actualColor = "black";
+let drawing = false;
 
+let board = document.querySelector("#canvas"); // selecting the drawing board
+let context = board.getContext("2d"); // this variable will give rendering in 2D to the <canvas> element
+// When I click and hold (mousedown), the drawing mode will be activated!
 // Events:
 document.querySelectorAll(".colors .color").forEach((item) => {
   item.addEventListener("click", colorClick);
 });
-
+board.addEventListener("mousedown", mouseDown);
 // Functions:
 function colorClick(e) {
   let color = e.target.getAttribute("id");
   console.log("color: " + color);
   actualColor = color;
 
-  // remove active class from black, and put the active class in other color:
   document.querySelector(".color.active").classList.remove("active");
   e.target.classList.add("active");
+}
+function mouseDown() {
+  console.log("clicked");
+  drawing = true;
 }
