a8bcf3d402377a42e4632ec2a573958309ee39ea
Carolina Strecht <carolina.stsampaio@gmail.com>
2021-12-07 18:35:38 +0000

    draw mode; UI update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let actualColor = "black";
 let drawing = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let board = document.querySelector("#canvas");
 let context = board.getContext("2d");
@@ -9,20 +11,48 @@ document.querySelectorAll(".colors .color").forEach((item) => {
   item.addEventListener("click", colorClick);
 });
 board.addEventListener("mousedown", mouseDown);
+board.addEventListener("mousemove", mouseMove);
 board.addEventListener("mouseup", mouseUp);
 // Functions:
 function colorClick(e) {
-  let color = e.target.getAttribute("id");
-  console.log("color: " + color);
-  actualColor = color;
+  let myColor = e.target.getAttribute("id");
+  actualColor = myColor;
 
   document.querySelector(".color.active").classList.remove("active");
   e.target.classList.add("active");
 }
-function mouseDown() {
+function mouseDown(e) {
   drawing = true;
+  mouseX = e.pageX - board.offsetLeft;
+  mouseY = e.pageY - board.offsetTop;
+}
+function mouseMove(e) {
+  if (drawing) {
+    draw(e.pageX, e.pageY);
+  }
 }
 function mouseUp() {
-  console.log("already clicked");
   drawing = false;
+}
+function draw(x, y) {
+  // get the inicial position
+  let pointX = x - board.offsetLeft;
+  let pointY = y - board.offsetTop;
+
+  // draw:
+  context.beginPath();
+  context.lineWidth = 8;
+  if (actualColor === "rgb(72, 117, 72)") {
+    context.lineWidth = 50;
+  }
+  context.lineJoin = "round";
+  context.moveTo(mouseX, mouseY);
+  context.lineTo(pointX, pointY);
+  context.closePath();
+  context.strokeStyle = actualColor;
+  context.stroke();
+
+  // save the position
+  mouseX = pointX;
+  mouseY = pointY;
 }
